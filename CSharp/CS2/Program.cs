1bc60f605f2cdf6c653e9e039d8e8a85ca627865
Pato570 <pgomezagui2009@gmail.com>
2026-09-24 21:00:54 -0700

    Add files via upload

---
+++ CSharp/CS2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System
+// Espacio de nombres
+namespace Name
+{
+
+}namespace CS2
+{
+    // Clase Principal
+    class Program
+    {
+        //Funcion principal
+        static void Main(string[] args)
+        {
+            // Sesión 5: Tipos de datos
+            // Sintaxis para declarar variables
+            //tipo_de_dato identificador_variable;
+            // 1. Entero
+            int a;
+            // 2. Cadena de texto
+            string s;
+            // 3.1 Flotante (precisión sencilla)
+            float f;
+            // 3.2 Flotante (precisión doble)   
+            double d;
+            // 4. Booleano (lógico)
+            bool b;
+            // Inicializaciones
+            a = 5;
+            s = "Exactas";
+            f = 8.5F;
+            d = 9.5;
+            b = true;
+            // Palabra reservada: Identificador especial predefinido para el compliador
+            //Interpolación: Combinación de datos dentro de una cadena
+            // Impresiones
+            Console.WriteLine($"Entero: {a}");
+            Console.WriteLine($"Flotante (precisón sencilla): {f}")
+            Console.WriteLine($"Flotante (precisón doble): {d}")
+            Console.WriteLine($"Cadena de texto: {5}")
+            Console.WriteLine($"Booleano: {b}")
+        }//Término de la función principal    
+    }// Término de la clase principal
+}//Término del espacio de nombres
---
+++ CSharp/CS3/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace CS3
+        class Program
+        static void Main(string[] args)
+{
+    {
+        {
+        static void Main(stringl[] args)
+            // Sesiion 6: Operadores
+            // Declaración e inicialización
+            double a = 1;
+            double b = 2;
+            double resultado = 0;
+            // 1. Operadores aritméticos
+            // a. Suma: +
+            resultado = a + b;
+            Console.WriteLine($"Resultado: (resultado}");
+            // b. Resta: -
+            resultado = a - b;
+            Console.WriteLine($"Resta: {resultado}");
+            // c. Multiplicación: *
+            resultado = a * b;
+            Console.WriteLine($"Resultado: {resultado}");
+            // d. División:/
+            resultado = a / b;
+            Console.WriteLine($"Resultado: {resultado}");
+            // e. Resto (módulo): %
+            resultado = a % b;
+
+            Console.WriteLine($"Resultado: {resultado}") ;
+            // Incrementos y decrementos
+            // resultado = resultado + 9;
+            resultado += 9;
+            resultado -= 5;
+            Console.WriteLine($"Resultado: {resultado}");
+            /*
+            2. Operadores comparativos
+            a. Igualdad: ==
+            b. Diferencia: !=
+            c. Menor que: <
+            d. Mayor que: ›
+            e. Menor o igual que: ‹=
+            f. Mayor o igual que: ›=
+            */
+        }// Término de la función principal
+    } // Término de la clase principal
+} // Término de la clase principal
